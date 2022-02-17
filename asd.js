1d99ff62673565284049303245f7ba7b255510f2
Huba <bende.huba@students.jedlik.eu>
2022-02-17 22:56:57 +0100

    latenightsex

--- asd.js
+++ asd.js
@@ -1,10 +1,16 @@
+//sleep funkció
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+//alap irodai dolgok
+
 var kamera = 0;
 function changeImage(a) {
     document.getElementById("img").src=a;
 };
 
-
-function setCam(){
+function setOffice(){
     if(kamera == 0){
         changeImage("/images/kozep/kozep office nyitva.png");
     }
@@ -21,21 +27,57 @@ function balra() {
     if(kamera < -1){
         kamera+= 1
     }
-    setCam();
+    setOffice();
 };
 function jobbra() {
     kamera += 1
     if(kamera > 1){
         kamera-= 1
     }
-    setCam();
+    setOffice();
 };
+
+//ide majd a jumpscare-k kellenek, mert most csak fel/le teker, de meg kell csinálni, hogy aktív kamerát adjon stb
+
+var canOpen = true;
+
+async function openTimeOut(){
+    await sleep(500);
+    canOpen = true;
+}
+
+function fel(){
+    if (canOpen == true){
+        location.hash = "#ontop";
+        canOpen = false;
+        openTimeOut();
+    }
+};
+
+function le(){
+    if (canOpen == true){
+        location.hash = "#ondown";
+        canOpen = false;
+        openTimeOut();
+    }
+};
+
+
+//38 fel - 40 le  (w, s működik, de nyilakkal nem :c) window eventek
 
 window.onkeydown = function(e) {
     if (e.keyCode == "39" || e.keyCode== "68"){
-        jobbra()
+        jobbra();
     }
     else if (e.keyCode == "37" || e.keyCode== "65"){
-        balra()
+        balra();
     }
-}
+    else if (e.keyCode == "38" || e.keyCode== "87"){
+        fel();
+    }
+    else if (e.keyCode == "40" || e.keyCode== "83"){
+        le();
+    }
+};
+
+window.addEventListener("wheel", e => e.preventDefault(), { passive:false });
